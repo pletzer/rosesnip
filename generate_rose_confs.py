95036ea2c81ef1cd6b449db376bd68aa6f0bb5dd
Alex Pletzer <alexander@gokliya.net>
2020-04-06 14:31:54 +1200

    now able to generate all the files

--- generate_rose_confs.py
+++ generate_rose_confs.py
@@ -9,10 +9,13 @@ import time
 import os
 import re
 
-
 """
 Generate rose configuration files for parallel processing
 """
+
+PAT_DIAG = re.compile(r'namelist:diags\(([^\)]+)\)')
+PAT_MODEL = re.compile(r'namelist:models\(([^\)]+)\)')
+
 
 
 def read_config(config_file=None):
@@ -46,7 +49,7 @@ def get_month_day(dt):
     """Extract the month and day from YYYY-MM-DD-*"""
     return dt.split('-')[1:3]
 
-def split_time_range(start_date, end_date, n):
+def split_time_range(start_date, end_date):
     """
     Split a time range into n segments
 
@@ -57,35 +60,33 @@ def split_time_range(start_date, end_date, n):
     """
     res = []
     sdt = start_date
-    print(f'**** start_date = {start_date}')
-    print(f'**** end_date = {end_date}')
     start_year = get_year(start_date)
     end_year = get_year(end_date)
-    dy = (end_year - start_year)//n
-    if dy == 0:
-        raise ValueError("ERROR: either decrease -n or increase the end date's year")
-    for i in range(1, n + 1):
-        if i == n:
-            edt = end_date
-        else:
-            edt = str(start_year  + i * dy) + '-01-01'   
-        res.append((sdt, edt))
-        sdt = edt
+    for y in range(start_year, end_year):
+
+        s = str(y) + '-01-01'
+        if y == start_year:
+            s = start_date
+
+        e = str(y +1) + '-01-01'
+        if y == end_year:
+            e = end_date
+
+        res.append((s, e))
+
     return res
 
 def generate_conf(rose_conf, start_date, end_date, model, diag, index):
 
-    conf = copy.deepcopy(rose_conf)
+    conf = config = ConfigParser()
+    for section in rose_conf.sections():
+        if not re.match(PAT_MODEL, section) and not re.match(PAT_DIAG, section):
+            conf[section] = rose_conf[section]
     mname = 'namelist:models(' + model + ')'
     dname = 'namelist:diags(' + diag + ')'
-    for section in conf.sections():
-        if section != 'general' and \
-               section != dname and \
-               section != mname:
-            # remove this section
-            print(f'*** removing section >{section}<')
-            del conf[section]
-    # set the start and send dates
+    conf[mname] = rose_conf[mname]
+    conf[dname] = rose_conf[dname]
+   # set the start and send dates
     conf[mname]['start_date'] = start_date
     conf[mname]['end_date'] = end_date
     # add processor Id
@@ -93,55 +94,34 @@ def generate_conf(rose_conf, start_date, end_date, model, diag, index):
 
     return conf
 
-def get_all_diags(rose_conf):
-    res = ''
-    pat = re.compile(r'namelist:diags\(([^\)]+)\)')
-    for section in rose_conf.sections():
-        m = re.match(pat, section)
-        if m:
-            res += ',' + m.group(1)
-    return res[1:]
 
-def get_all_models(rose_conf):
+def get_all_sections_of_type(rose_conf, pat):
     res = ''
-    pat = re.compile(r'namelist:models\(([^\)]+)\)')
     for section in rose_conf.sections():
         m = re.match(pat, section)
         if m:
             res += ',' + m.group(1)
-    return res[1:]
+    return res[1:].split(',')
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='Generate parallel rose config files.')
-    parser.add_argument('--models', dest='models', default='', 
-                           help='specify comma separated list of models, for instance "u-bc179,u-bc292 (leave empty to include all models)"')
-    parser.add_argument('--diags', dest='diags', default='', 
-                           help='specify comma separated list of diagnostics, for instance "tas_global (leave empty to consider include diagnostics)"')
-    parser.add_argument('--configuration', dest='conf_filename', default='rose-app-expanded.conf', help='serial rose config file, for instance "rose-app-expanded.conf"')
-    parser.add_argument('--num_procs', dest='num_procs', default=1, type=int, help='number of processors')
-    parser.add_argument('--output_dir', dest='output_dir', default='', help='specify output directory')
-    parser.add_argument('--clear', dest='clear', action='store_true', help='start by removing files in output directory')
+    parser.add_argument('-c', dest='conf_filename', default='rose-app-expanded.conf', 
+                           help='serial rose config file, for instance "rose-app-expanded.conf"')
+    parser.add_argument('-o', dest='output_dir', default='', help='specify output directory')
+    parser.add_argument('-C', dest='clear', action='store_true', help='start by removing any files in output directory')
     args = parser.parse_args()
 
     # read the configuration file
     rose_conf = read_config(args.conf_filename)
 
-    if not args.models:
-        args.models = get_all_models(rose_conf)
-
-    if not args.diags:
-        args.diags = get_all_diags(rose_conf)
+    models = get_all_sections_of_type(rose_conf, PAT_MODEL)
+    diags = get_all_sections_of_type(rose_conf, PAT_DIAG)
 
     print('configuration file: {}'.format(args.conf_filename))
-    print('models            : {}'.format(args.models.split(',')))
-    print('diags             : {}'.format(args.diags.split(',')))
-    print('num processors    : {}'.format(args.num_procs))
-
-    # run some checks
-    if args.num_procs < 1:
-        raise ValueError('ERROR: num processors {} < 1!'.format(args.num_procs))
+    print('models            : {}'.format(models))
+    print('diags             : {}'.format(diags))
 
     if not args.output_dir:
         # generate name for temporary directory
@@ -156,13 +136,13 @@ def main():
                 os.remove(f)
 
     index = 0
-    for diag in args.diags.split(','):
+    for diag in diags:
         diag_def = rose_conf['namelist:diags(' + diag + ')']
-        for model in args.models.split(','):
+        for model in models:
             model_def = rose_conf['namelist:models(' + model + ')']
             start_date = model_def['start_date']
             end_date = model_def['end_date']
-            for sdt, edt in split_time_range(start_date, end_date, args.num_procs):
+            for sdt, edt in split_time_range(start_date, end_date):
                 conf = generate_conf(rose_conf, start_date, end_date, model, diag, index)
                 # write the file
                 confilename = os.path.join(args.output_dir, args.conf_filename + f'_{index}')
