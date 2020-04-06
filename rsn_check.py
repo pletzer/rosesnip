e9dda39d9826bed3ae45050caec02e6f4b5f8b3c
Alex Pletzer <alexander@gokliya.net>
2020-04-07 10:16:08 +1200

    bug fixes

--- rsn_check.py
+++ rsn_check.py
@@ -21,15 +21,22 @@ def main():
     csvfile = os.path.join(args.result_dir, csvfile)
 
 
+    try:
+        df = pandas.read_csv(csvfile)
+    except:
+        # something bad happened
+        print('ERROR: Could not read {}. Maybe you forgot to submit the jobs (rsn_submit)?'.format(csvfile))
+        sys.exit(1)
+
     for job_id in df.loc[df.status == 'allocated', 'job_id']:
         cmd = 'sacct -j {}'.format(job_id)
         out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
         if re.search(completed_pat, out):
             df.loc[df.job_id == job_id, 'status'] = 'completed'
 
-    df = pandas.read_csv(csvfile)
+
     for st in 'UNKOWN', 'queued', 'allocated', 'completed':
-        n = len(df.loc[df.status == 'UNKNOWN'].index)
+        n = len(df.loc[df.status == st].index)
         print('Number of {} jobs: {}'.format(st, n))
  
 
--- rsn_submit.py
+++ rsn_submit.py
@@ -10,11 +10,15 @@ from datetime import datetime
 Submit the previously generated rose-configuration files
 """
 
-queued_pat = re.compile('job (\d+) queued')
-allocated_pat = re.compile('job (\d+) has been allocated')
+queued_pat = re.compile(r'job (\d+) queued')
+allocated_pat = re.compile(r'job (\d+) has been allocated')
 
 
 def get_job_id(output):
+
+    # convert byte to string (required by Python3)
+    output = output.decode("utf-8")
+
     stat = 'UNKNOWN'
     job_id = -1
 
@@ -38,19 +42,33 @@ def main():
     parser.add_argument('-t', dest='time', default='00:30:00', help='max time for submission jobs')
     parser.add_argument('-m', dest='mem', default='1g', help='max memory footprint of submission jobs')
     parser.add_argument('-a', dest='abrun', default='./turbofan/trunk/bin/abrun.sh', help='path to abrun.sh')
+    parser.add_argument('-T', dest='test', action='store_true', help='test only, do not submit jobs')
     args = parser.parse_args()
 
     csvfile = 'status.csv'
-    csv = open(csvfile, 'w')
+    csv = open(os.path.join(args.result_dir, csvfile), 'w')
     csv.write('rose_conf_filename,job_id,status,submission_date,submission_time\n')
 
-    for conffile in glob.glob(os.path.join(args.result_dir, '*.conf_[0-9]+'):
-        cmd = 'echo srun --output={args.result_dir}/slurm-%a.out --time={args.time} --mem={args.mem} {args.abrun} {args.monitorModel} -c {conffile} -v'
+    for conffile in glob.glob(args.result_dir + '/*.conf_[0-9]*'):
+        slurm_options = '--output={}/slurm-%a.out --time={} --mem={}'.format(args.result_dir, args.time, args.mem)
+
+        # build the command
+        cmd = ''
+        if args.test:
+            # testing only
+            cmd = 'echo '
+        cmd += 'srun {} {} {} -c {} -v'.format(slurm_options, args.abrun, args.monitorModel, conffile)
+
+        # execute the command
         out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
-        jobid, stat = get_job_id(out)
+
+        # get the job_id and status of the job
+        job_id, stat = get_job_id(out)
+
+        # store
         dt = datetime.now()
-        now_date = '{}-{}-{}'.format(dt.year, dt.month, dt.day)
-        now_time = '{}:{}:{}'.format(dt.hour, dt.minute, dt.second)
+        now_date = '{}-{:02}-{:02}'.format(dt.year, dt.month, dt.day)
+        now_time = '{:02}:{:02}:{:02}'.format(dt.hour, dt.minute, dt.second)
         csv.write('{},{},{},{},{}\n'.format(conffile, job_id, stat, now_date, now_time))
 
     csv.close()
