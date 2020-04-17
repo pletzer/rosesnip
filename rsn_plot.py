965fb2a394be470580cbc4116cb70e09c56db581
Alex Pletzer <alexander@gokliya.net>
2020-04-17 05:51:16 +0000

    fixed

--- rsn_plot.py
+++ rsn_plot.py
@@ -12,45 +12,28 @@ from matplotlib import pylab
 
 def get_model_diag_from_filename(ncfile):
     bn = os.path.basename(ncfile).split('_')
-    model, diag = bn[0], bn[1]
+    model, diag = bn[0], bn[2]
     return model, diag
 
 def get_diag2model_map(ncfiles):
     d = {}
     for ncfile in ncfiles:
         model, diag = get_model_diag_from_filename(ncfile)
-        d[diag] = d.get(diag, []).append((model, ncfile))
+        v = d.get(diag, []) + [(model, ncfile)]
+        d[diag] = v
     return d
 
-def get_time_series_data_from_netcdf_file(ncfile, model):
+def get_time_series_data_from_netcdf_file(ncfile, diag):
     nc = netCDF4.Dataset(ncfile, 'r')
-    y = nc.variables[model][:]
+    print(ncfile)
+    y = nc.variables[diag][:]
     x = nc.variables['time'][:]
     nc.close()
     return x, y
 
-def get_properties(conf_file, diag, model):
-    props = {
-        'marker_edge_color': 'k',
-        'marker_face_color': 'r',
-        'marker_style': 'x',
-        'marker_size': 12,
-        'line_color' : 'b',
-        'line_width' : 1,
-        'line_style' : '-',
-    }
-    cf = ConfigParser()
-    cf.read(conf_file)
-    diag_defn = cf['namelist:diags(' + diag + ')']
-    for p in props:
-        props[p] = diag_defn.get(p, props[p])
-    return props
-
 
 def main():
     parser = argparse.ArgumentParser(description='Plot results.')
-    parser.add_argument('-c', dest='conf_filename', default='rose-app-expanded.conf', 
-                           help='serial rose config file, for instance "rose-app-expanded.conf"')
     parser.add_argument('-d', dest='result_dir', default='', help='specify result directory')
     args = parser.parse_args()
 
@@ -60,21 +43,33 @@ def main():
 
     # find all the onetcdf files
     ncfiles = glob.glob(args.result_dir + '/nc/*.nc')
+    print('Looking at netCDF files {}...'.format([os.path.basename(ncf) for ncf in ncfiles]))
 
 
     # create list of diagnostics
     diag_map = get_diag2model_map(ncfiles)
+    print(diag_map)
+
+    linecolor = ['-', '-.', '--', ':', ]
+    nline = len(linecolor)
+    markertype = ['x', '+', '*', '<', '>', 's', , 'D']
+    nmark = len(markertype)
 
     # generate plots, one for each diagnostic
     for diag, mf in diag_map.items():
         legs = []
+
+        count = 0
         for model, ncfile in mf:
-            x, y = get_time_series_data_from_netcdf_file(ncfile, model)
-            props = get_properties(args.conf_filename, diag, model)
-            pylab.plot(x, y, **props)
+            x, y = get_time_series_data_from_netcdf_file(ncfile, diag)
+            pylab.plot(x, y, linecolor[count % nline] + markertype[count % nmark])
             legs.append(model)
+            count += 1
+            
         pylab.title(diag)
-        pyalb.legend(legs)
-        pylab.figsave(args.result_dir + '/images/{}_{}.png')
+        pylab.legend(legs)
+        pylab.savefig(args.result_dir + '/images/{}.png'.format(diag))
 
 
+if __name__ == '__main__':
+    main()
